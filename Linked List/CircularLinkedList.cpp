2c22dbcfb840c23236e2c7ad6dcc733e9e4f6606
adarshgupta404 <112967201+adarshgupta404@users.noreply.github.com>
2022-09-06 22:23:53 +0530

    Add files via upload

---
+++ Linked List/CircularLinkedList.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+class Node
+{
+    public:
+        int data;
+        Node* next;
+        Node(int d)
+        {
+            this->data = d;
+            this->next = NULL;
+        }
+        ~Node()
+        {
+            int val = this->data;
+            if (next != NULL)
+            {
+                delete next;
+                next = NULL;
+            }
+            cout << "Memory free for node with data " << val << endl;
+        }
+};
+int main()
+{
+
+
+
+getch();
+return 0;
+}
