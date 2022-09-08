53053247a1d6f8c75198fa9c6afd190d5cbc26c5
Adarsh <gadarsh747@gmail.com>
2022-09-08 12:11:54 +0530

    Update CircularLinkedList.cpp

--- Linked List/CircularLinkedList.cpp
+++ Linked List/CircularLinkedList.cpp
@@ -101,64 +101,6 @@ void InsertAtPos(Node *&tail)
         tail =  tail->next; 
 }
 
-void deleteNode(Node *&head, int pos)
-{
-    if (pos == 1)
-    {
-        Node *temp = head;
-        head = head->next;
-        temp->next = NULL;
-        delete temp;
-    }
-    else
-    {
-        Node *prev = NULL;
-        Node *curr = head;
-        int cnt = 2;
-        while (cnt <= pos)
-        {
-            prev = curr;
-            curr = curr->next;
-            cnt++;
-        }
-        prev->next = curr->next;
-        curr->next = NULL;
-        delete curr;
-    }
-}
-
-void search(Node *&head, int d)
-{
-    Node *p = head;
-    int cnt = 0;
-    while (p != NULL)
-    {
-        cnt++;
-        if (p->data == d)
-        {
-            printf("%d is present at postion %d.\n", d, cnt);
-            return;
-        }
-        p = p->next;
-    }
-    cout << "\nNot Present.\n";
-}
-
-
-void reverse(Node *&head)
-{
-    Node *before = NULL;
-    Node *after = NULL;
-    while (head != NULL)
-    {
-        after = head->next;
-        head->next = before;
-        before = head;
-        head = after;
-    }
-    head = before;
-}
-
 int No_of_Nodes(Node *&tail)
 {
     int cnt = 1;
@@ -171,6 +113,94 @@ int No_of_Nodes(Node *&tail)
     return cnt;
 }
 
+
+void delete_firstNode(Node* &tail)
+{
+    Node* temp = tail->next;
+    tail->next = temp->next;
+    free(temp);
+    temp = NULL;
+}
+
+void delete_lastNode(Node* &tail)
+{
+    Node* temp = tail->next;
+    while (temp->next != tail)
+    {
+        temp = temp->next;
+    }
+    temp->next = tail->next;
+    free(tail);
+    tail = temp;
+}
+
+void delete_intermediate(Node* &tail)
+{
+    int pos;
+    printf("\nEnter the position you want to delete: ");
+    scanf("%d", &pos);
+    if(pos == 1)
+    {
+        delete_firstNode(tail);
+        return;
+    }    
+    
+    int l = No_of_Nodes(tail);
+    if(pos==l)
+    {
+        delete_lastNode(tail);
+        return;
+    }
+    
+    Node* temp = tail->next;
+    while (pos>2)  
+    {
+        temp = temp->next;
+        pos--;
+    }
+    Node* temp2 = temp->next;
+    temp->next = temp2->next;
+    if(temp2 == tail)
+        tail = temp;
+    free(temp2);
+    temp2 = NULL;
+}
+
+void search(Node *&tail, int d)
+{
+    Node *p = tail->next;
+    int cnt = 0;
+
+    do
+    {
+        cnt++;
+        if (p->data == d)
+        {
+            printf("%d is present at postion %d.\n", d, cnt);
+            return;
+        }
+        p = p->next;
+    }while (p != tail->next);
+    cout << "Not Present.\n";
+}
+
+
+void reverse(Node *&tail)
+{
+    Node* currN, *prevN, *nextN;
+    currN = tail->next;
+    nextN = currN->next;
+    while (currN!=tail)
+    {
+        prevN = currN;
+        currN = nextN;
+        nextN = currN->next;
+        currN->next = prevN;
+    }
+    nextN->next = tail;
+    tail = nextN;
+}
+
 int main()
 {
     int d;
@@ -179,14 +209,15 @@ int main()
     cout << "LINKED LIST MENU" << endl
              << "1.CREATE" << endl
              << "2.DISPLAY" << endl
-             << "3.INSERT_END" << endl
+             << "3.INSERT_ENDING" << endl
              << "4.INSERT_BEGINNING" << endl
              << "5.INSERT BETWEEN" << endl
-             << "6.DELETE" << endl
-             << "7.SEARCH" << endl
-             << "8.SORT" << endl
-             << "9.REVERSE" << endl
-             << "10.NO OF NODES" << endl
+             << "6.DELETE_BEGINNING" << endl
+             << "7.DELETE_ENDING" << endl
+             << "8.DELETE_INTERMEDIATE" << endl
+             << "9.NO. OF NODES" << endl
+             << "10.REVERSE" << endl
+             << "11.NO OF NODES" << endl
              << "Any key-EXIT" << endl;
     while (true)
     {
@@ -203,34 +234,42 @@ int main()
             break;
         case 3:
             InsertAtTail(tail);
+            display(tail);
             break;
         case 4:
             InsertAtHead(tail);
+            display(tail);
             break;
         case 5:
             InsertAtPos(tail);
+            display(tail);
             break;
-        /*case 6:
-            printf("Enter the position of element to be delete: ");
-            int pos;
-            scanf("%d", &pos);
-            deleteNode(head, pos);
+        case 6:
+            delete_firstNode(tail);
+            display(tail);
             break;
-        /*case 7:
-            cout << "Enter the element to be searched: ";
-            int num;
-            cin >> num;
-            search(head, num);
-            break;*/
+        case 7:
+            delete_lastNode(tail);
+            display(tail);
+            break;
         case 8:
-            cout << "No of Nodes in the linked list is " << No_of_Nodes(tail) << "." << endl;
+            delete_intermediate(tail);
+            display(tail);
             break;
         case 9:
+            cout << "Number of Nodes in the linked list is " << No_of_Nodes(tail) << "." << endl;
+            break;
+        case 10:
+            cout<<"After Reversing: ";
             reverse(tail);
             display(tail);
             break;
-        case 10:
-            
+        case 11:
+            cout << "Enter the element to be searched: ";
+            int num;
+            cin >> num;
+            search(tail, num);
+            break;
         default:
             goto end;
         }
